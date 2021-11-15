20df4a2ae33810cb4364dc5676dcb4685614af45
JackyKeng <jianjackkeng@gmail.com>
2021-11-15 11:17:22 +0800

    fb custom login button

--- App.js
+++ App.js
@@ -5,34 +5,42 @@ import {
   AccessToken,
   GraphRequest,
   GraphRequestManager,
+  LoginManager,
 } from 'react-native-fbsdk';
+import {Button} from 'react-native-paper';
+
+const fbLogin_Click = () => {
+  LoginManager.logInWithPermissions(['public_profile']).then(
+    function (result) {
+      if (result.isCancelled) {
+        console.log('Login cancelled');
+      } else {
+        console.log(result);
+        AccessToken.getCurrentAccessToken().then(data => {
+          console.log(data);
+          const infoRequest = new GraphRequest(
+            '/me?fields=name,email,picture.type(large)',
+            null,
+            responseInfoCallback,
+          );
+          // Start the graph request.
+          new GraphRequestManager().addRequest(infoRequest).start();
+        });
+      }
+    },
+    function (error) {
+      console.log('Login fail with error: ' + error);
+    },
+  );
+};
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Hello World</Text>
-      <LoginButton
-        permissions={['email']}
-        onLoginFinished={(error, result) => {
-          if (error) {
-            console.log('login has error: ' + result.error);
-          } else if (result.isCancelled) {
-            console.log('login is cancelled.');
-          } else {
-            AccessToken.getCurrentAccessToken().then(data => {
-              console.log(data);
-              const infoRequest = new GraphRequest(
-                '/me?fields=name,email,picture.type(large)',
-                null,
-                responseInfoCallback,
-              );
-              // Start the graph request.
-              new GraphRequestManager().addRequest(infoRequest).start();
-            });
-          }
-        }}
-        onLogoutFinished={() => console.log('logout.')}
-      />
+      <Button icon="facebook" mode="outlined" onPress={fbLogin_Click}>
+        Login
+      </Button>
     </SafeAreaView>
   );
 };
